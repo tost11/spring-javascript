8184f9b471b83a874d22b89a77a0c9b75e6d2edf
44616E69656C <77584851+44616E69656C@users.noreply.github.com>
2022-07-18 15:06:51 +0200

    while-schleife nach oben gesetzt

--- python/text.py
+++ python/text.py
@@ -29,17 +29,17 @@ signal(SIGINT, signal_handler)
 print("\n\nCheck for data")
 
 client = getClient()
-if client.connect():
-    print("connect")
+while isrunning:
+    if client.connect():
+        print("connect")
 
-    result = client.read_input_registers(0x3100, 19, unit=1)
-    if isinstance(result, Exception):
-        print("Got exception reading 0x3100 - 0x3118")
-        print(result)
-    else:
-        if result.function_code < 0x80:
-            print(result.registers)
-            while isrunning:
+        result = client.read_input_registers(0x3100, 19, unit=1)
+        if isinstance(result, Exception):
+            print("Got exception reading 0x3100 - 0x3118")
+            print(result)
+        else:
+            if result.function_code < 0x80:
+                print(result.registers)
                 time.sleep(5)
                 try:
                     data = {'inputVoltage': result.registers[0]/100,
@@ -51,10 +51,12 @@ if client.connect():
                     response = requests.post('http://localhost:8080/api/data/add', json = data)
                 except:
                     print("Bad Request")
-        else:
-            print("Unable to read 0x3100 - 0x3112")
+            else:
+                print("Unable to read 0x3100 - 0x3112")
 
-    client.close()
+        client.close()
+    else:
+        sleep(5)
 
 """
 inputVoltage
