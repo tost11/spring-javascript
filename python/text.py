0382a9b0b7a67567e7daa07c31a24d3adda7ad5f
44616E69656C <77584851+44616E69656C@users.noreply.github.com>
2022-07-18 13:37:49 +0200

    Request post

---
+++ python/text.py
@@ -0,0 +1,62 @@
+from pymodbus.client.sync import ModbusSerialClient as ModbusClient
+from pymodbus.exceptions import ModbusIOException
+from pymodbus.mei_message import ReadDeviceInformationRequest
+from pymodbus.constants import DeviceInformation
+import traceback
+import time
+import requests
+from signal import signal, SIGINT
+from sys import exit
+
+isrunning = True
+
+def getClient():
+    return ModbusClient(
+        method = "rtu",
+        port = "/dev/tty.usbserial-AB0L19WE",
+        #port = "/dev/ttyUSB0",
+        baudrate = 115200,
+        timeout = 1
+    )
+def signal_handler(sig, frame):
+    global isrunning
+    print("Ctrl + C pressed")
+    isrunning = False
+
+
+signal(SIGINT, signal_handler)
+print("\n\nCheck for data")
+
+client = getClient()
+if client.connect():
+    print("connect")
+
+    result = client.read_input_registers(0x3100, 19, unit=1)
+    if isinstance(result, Exception):
+        print("Got exception reading 0x3100 - 0x3118")
+        print(result)
+    else:
+        if result.function_code < 0x80:
+            print(result.registers)
+            while isrunning:
+                time.sleep(5)
+                try:
+
+                    response = requests.post('localhost:9000/api/data/add', data={'inputVoltage': result.registers[0]/100,
+                                                                                  'inputAmpere': result.registers[1]/100,
+                                                                                  'batteryVoltage': result.registers[4]/100,
+                                                                                  'outputAmpere': result.registers[5]/100 - result.registers[13]/100
+                                                                                  })
+                except:
+                    print("Bad Request")
+        else:
+            print("Unable to read 0x3100 - 0x3112")
+
+    client.close()
+
+"""
+inputVoltage
+inputAmpere
+batteryVoltage
+outputAmpere
+"""
