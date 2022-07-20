d3126f466d3b2f551b561a7acf19bcea4a061c7b
helenxv <helen.noske@gmail.com>
2022-07-20 10:29:42 +0200

    Graphen für Solardaten fast fertig

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,15 +8,42 @@ let zeitraum=0;
 
 window.onload = function () {
   console.log("Hello World!")
+    latestData()
+  //graph1();
   //pageValues();
   graph1();
   graph2();
 }
 
-window.getProduktion=async()=> {
+window.latestData = async () => {
+    const response = await fetch("/api/data/get/latest", {
+        method: 'GET',
+        headers: {
+            'Conent-Type': 'application/json'
 
+        }
+    });
+    const myJson = await response.json()
+    console.log("Daten:", myJson)
 
-  const response=await fetch("/api/data/get/"+id2, {
+  const node = document.getElementById("AP")
+  node.innerText ="Aktuelle Produktion: " + myJson.inputVoltage * myJson.inputAmpere + "W"
+  const node1 = document.getElementById("AV")
+  node1.innerText ="Aktueller Verbrauch: " + myJson.batteryVoltage * myJson.outputAmpere + "W"
+  const node2 = document.getElementById("BV")
+  node2.innerText ="Batteriespannung: " + myJson.batteryVoltage + "V"
+
+    const lu = document.getElementById("werte")
+    lu.appendChild(node)
+    lu.appendChild(node1)
+    lu.appendChild(node2)
+
+}
+
+setInterval(latestData , 10000 );
+
+window.getProduktion = async () =>{
+  const response = await fetch("/api/data/get/latest", {
     method: 'GET',
 
     headers: {
@@ -24,62 +51,42 @@ window.getProduktion=async()=> {
 
     }
   });
+  const myJson = await response.json()
+  console.log("myJsom:", myJson.inputVoltage * myJson.inputAmpere)
+  const p = myJson.inputVoltage * myJson.inputAmpere
 
-  const myJson = await response.json();
-  //console.log((myJson.datenlist[id2].inputVoltage) * (myJson.datenlist[id2].inputAmpere))
-  return (myJson[id2].inputVoltage) * (myJson[id2].inputAmpere);
+  return p;
 }
 
-/*window.pageValues=async()=> {
-
-  const response = await fetch("/api/data/get/" + id, {
+window.getBatteryVoltage = async () =>{
+  const response = await fetch("/api/data/get/latest", {
     method: 'GET',
-
     headers: {
       'Conent-Type': 'application/json'
 
     }
   });
-  id ++;
-  const myJson = await response.json();
-  console.log("Daten:", myJson);
+  const myJson = await response.json()
+  console.log("myJsom:", myJson.batteryVoltage)
+  const bv = myJson.batteryVoltage
 
-  for (let i = 0; i < myJson.datenlist.length; i++) {
-
-    const node = document.createElement("li")
-    const node1 = document.createElement("li")
-    const node2 = document.createElement("li")
-    const node3 = document.createElement("li")
-
-    const text = document.createTextNode((myJson.datenlist[i].inputVoltage) * (myJson.datenlist[i].inputAmpere))
-    const text1 = document.createTextNode((myJson.datenlist[i].batteryVoltage) * (myJson.datenlist[i].outputAmpere))
-    const text2 = document.createTextNode(myJson.datenlist[i].batteryVoltage)
-
-
-    const schrift = document.createTextNode("Aktuelle Produktion: ")
-    const schrift1 = document.createTextNode("Aktueller Verbrauch: ")
-    const schrift2 = document.createTextNode("Batteriespannung: ")
-    const text3 = document.createTextNode("Graph: ")
-
-    node.appendChild(schrift);
-    node1.appendChild(schrift1)
-    node2.appendChild(schrift2)
-
-    node.appendChild(text)
-    node1.appendChild(text1)
-    node2.appendChild(text2)
-    node3.appendChild(text3)
-
-    document.getElementById("werte").appendChild(node);
-    document.getElementById("werte").appendChild(node1);
-    document.getElementById("werte").appendChild(node2);
-    document.getElementById("werte").appendChild(node3);
-
-  }
+  return bv;
 }
-*/
 
+window.getVerbrauch = async () =>{
+  const response = await fetch("/api/data/get/latest", {
+    method: 'GET',
+    headers: {
+      'Conent-Type': 'application/json'
 
+    }
+  });
+  const myJson = await response.json()
+  console.log("myJsom:", myJson.batteryVoltage * myJson.outputAmpere)
+  const v = myJson.batteryVoltage * myJson.outputAmpere
+
+  return v;
+}
 
 
   function graph1() {
@@ -101,7 +108,7 @@ window.getProduktion=async()=> {
 
     // create options object with titles, colors, etc.
     let options = {
-      title: "Solardaten",
+      title: "Aktuelle Solardaten",
       hAxis: {
         title: "Time"
       },
@@ -118,12 +125,17 @@ window.getProduktion=async()=> {
     // interval for adding new data every 250ms
     let index = 0;
     id2=0;
-    setInterval(function () {
+    let counter =0;
+    setInterval(async function () {
       // instead of this random, you can make an ajax call for the current cpu usage or what ever data you want to display
-      let random = Math.random() * 30 + 20;
-      let random2=Math.random()*10+5;
+      const wert1 = await getProduktion();
+      const wert2 = await getVerbrauch();
+      counter++;
+      if(counter===400){
+        graph1();
+      }
       id2++;
-          data.addRow([index, random, random2]);
+          data.addRow([index, wert1, wert2]);
       chart.draw(data, options);
       index++;
     }, 500);
@@ -167,13 +179,19 @@ function batteryChart() {
   // interval for adding new data every 250ms
   let index = 0;
   id3 = 0;
-  setInterval(function () {
+  let counter=0;
+  setInterval(async function () {
     // instead of this random, you can make an ajax call for the current cpu usage or what ever data you want to display
-    let random = Math.random() * 30 + 20;
-
+    const wert = await getBatteryVoltage();
+    console.log("BV:", wert)
+    counter++;
+    if (counter === 400) {
+      graph2();
+    }
     id3++;
-    data.addRow([index, random]);
+    data.addRow([index, wert]);
     chart.draw(data, options);
+
     index++;
   }, 500);
 }
@@ -218,15 +236,67 @@ function oldValues() {
   chart.draw(data, options);
   // interval for adding new data every 250ms
   let index = 0;
-  id4=0;
+  id4 = 0;
   setInterval(function () {
     // instead of this random, you can make an ajax call for the current cpu usage or what ever data you want to display
     let random = Math.random() * 30 + 20;
-    let random2=Math.random()*10+5;
+    let random2 = Math.random() * 10 + 5;
     id4++;
     data.addRow([index, random, random2]);
     chart.draw(data, options);
     index++;
   }, 500);
 
+
 }
+
+
+/*
+window.pageValues=async()=> {
+ let id = 1;
+ const response = await fetch("/api/data/get/" + id, {
+   method: 'GET',
+
+   headers: {
+     'Conent-Type': 'application/json'
+
+   }
+ });
+
+ const myJson = await response.json();
+ console.log("Daten:", myJson);
+
+ for (let i = 0; i < myJson.datenlist.length; i++) {
+
+   const node = document.createElement("li")
+   const node1 = document.createElement("li")
+   const node2 = document.createElement("li")
+   const node3 = document.createElement("li")
+
+   const text = document.createTextNode((myJson.datenlist[i].inputVoltage) * (myJson.datenlist[i].inputAmpere))
+   const text1 = document.createTextNode((myJson.datenlist[i].batteryVoltage) * (myJson.datenlist[i].outputAmpere))
+   const text2 = document.createTextNode(myJson.datenlist[i].batteryVoltage)
+
+
+   const schrift = document.createTextNode("Aktuelle Produktion: ")
+   const schrift1 = document.createTextNode("Aktueller Verbrauch: ")
+   const schrift2 = document.createTextNode("Batteriespannung: ")
+   const text3 = document.createTextNode("Graph: ")
+
+   node.appendChild(schrift);
+   node1.appendChild(schrift1)
+   node2.appendChild(schrift2)
+
+   node.appendChild(text)
+   node1.appendChild(text1)
+   node2.appendChild(text2)
+   node3.appendChild(text3)
+
+   document.getElementById("werte").appendChild(node);
+   document.getElementById("werte").appendChild(node1);
+   document.getElementById("werte").appendChild(node2);
+   document.getElementById("werte").appendChild(node3);
+
+ }
+}
+*/
